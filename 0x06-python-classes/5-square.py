0f1d1fd889456ac1aacb3bd843de29ce0c1bacf3
Feke_21 <favouredubio@gmail.com>
2023-03-28 23:41:30 +0000

    Writes a class square based by 4-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+"""Defines a class named Square"""
+
+
+class Square:
+    """Defines a function called __init__"""
+    def __init__(self, size):
+        """Initializes size of self with size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Returns __size of self"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """If statement"""
+        if type(value) != int:
+            """Raise an error"""
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """Raise an error"""
+            raise ValueError("size must be >= 0")
+        else:
+            """Initialize __size of self with value"""
+            self.__size = value
+    """Defines area function or public instance method"""
+    def area(self):
+        """Returns area"""
+        return self.__size ** 2
+    """Defines a print function"""
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
