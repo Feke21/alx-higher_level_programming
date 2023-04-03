240ff0668e27aeba37a41f9194c360f3ec7ae556
Feke_21 <favouredubio@gmail.com>
2023-04-03 23:56:38 +0000

    Rectangle class based on 6-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,10 +3,11 @@
 
 
 class Rectangle:
+    """ Rectangle class attributes """
     number_of_instances = 0
     print_symbol = '#'
 
-    """ __init__ func """
+    """ Defines __init__ function """
     def __init__(self, width=0, height=0):
         """ Initialize width and height """
         self.width = width
