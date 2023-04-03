c009fd7ea314b88f8f1707739b79f0eef0aad311
Feke_21 <favouredubio@gmail.com>
2023-04-03 18:37:48 +0000

    Defines a rectangle based on 2-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Defines a rectangle class """
+
+
+class Rectangle:
+    """ Defines a func called __init__ """
+    def __init__(self, width=0, height=0):
+        """ Initializes the width and height with self """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Gets width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ If statement """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Gets height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ If statement """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Returns area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Returns perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ Returns rectangle in hash char form """
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        res = ''
+        for i in range(self.__height):
+                for j in range(self.__width):
+                    print("#", end="")
+                print()
+        return res[:-1]
