9cc6810dde81c2c4b20a1c437087599db156992c
Feke_21 <favouredubio@gmail.com>
2023-04-11 14:37:25 +0000

    True if obj is an instane of a_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" is_same_class module """
+
+
+def is_same_class(obj, a_class):
+    """ return true if obj is an instance of class, else False """
+    return True if type(obj) is a_class else False
