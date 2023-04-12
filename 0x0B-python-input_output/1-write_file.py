9d5c16fe840ffb614ce1219cf038d24ca71fbf2b
Feke_21 <favouredubio@gmail.com>
2023-04-12 13:09:07 +0000

    Write into a file and return num of char written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file and returns the num of characters written """
+    """
+    writes a string to a text file and
+    returns the num of characters written
+    """
     with open(filename, mode="w", encoding="UTF-8") as f:
         return (f.write(text))
