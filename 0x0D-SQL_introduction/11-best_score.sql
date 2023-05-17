be6cd017ecb853939be700e1eabf3cf663d76951
Feke_21 <favouredubio@gmail.com>
2023-05-17 14:19:25 +0000

    Fixing file

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with score >= 10 in second_table of database hbtn_0c_0
-SELECT score, name FROM second_tab le WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
