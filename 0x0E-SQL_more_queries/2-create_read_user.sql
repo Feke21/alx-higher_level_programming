8e2a83e24421b09830b8208b07211e1149da1a5c
Feke_21 <favouredubio@gmail.com>
2023-05-18 12:51:29 +0000

    Fixing errors

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,6 @@
 -- create database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user_0d_2 with passwd
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
-SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant select privileges in database hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON `hbtn_0d_2.`* TO 'user_0d_2'@'localhost';
