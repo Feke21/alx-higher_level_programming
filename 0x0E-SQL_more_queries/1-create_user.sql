f9f5b1174e554b76c7cfd4d974b8837a5f5358ab
Feke_21 <favouredubio@gmail.com>
2023-05-18 12:45:16 +0000

    Fixing error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create MySQL server user, user_0d_1
 -- create user_0d_1
-CREATE USER IF NOT EXISTSuser_0d_1@localhost IDENTIFIED by 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED by 'user_0d_1_pwd';
 -- grant all privileges
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
