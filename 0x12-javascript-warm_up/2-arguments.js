2e434f98abb308e794c0d5548b26ebde55dfd6a4
Feke_21 <favouredubio@gmail.com>
2023-06-13 23:56:18 +0000

    Fixing code structure

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
-#!/isr/bin/node
+#!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
