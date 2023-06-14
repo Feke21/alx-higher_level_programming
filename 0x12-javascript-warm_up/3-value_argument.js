1418b7a3b052ce942e7efd199aadb391447b9750
Feke_21 <favouredubio@gmail.com>
2023-06-14 00:01:00 +0000

    Fixing code to print exact

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const argument = process.argv[2];
+const arg = process.argv[2];
 
-if (typeof argument === 'undefined')
-{
+if (typeof arg === 'undefined') {
 	console.log('No argument');
-}
-else
-{
-	console.log(argument);
+} else {
+	console.log(arg);
 }
