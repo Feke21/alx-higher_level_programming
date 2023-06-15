5c62c294b73a07eecadea53a80682c8cc87c0675
Feke_21 <favouredubio@gmail.com>
2023-06-15 21:12:08 +0000

    Fixing size file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2])));
+const argv1 = process.argv[2];
 
-if (isNaN(size))
-{
-	console.log('Missing size'');
-}
-else
-{
-	for (let r = 0; r < size; r++)
-	{
-		let row = '';
-		for (let c = 0; c < size; c++) row += 'X';
-		console.log(row);
-	}
+if (!Number(argv1)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < argv1; i++) {
+    let output = '';
+    for (let j = 0; j < argv1; j++) {
+      output += 'X';
+    }
+    console.log(output);
+  }
 }
