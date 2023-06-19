0531ae6934613bd1b63740c0b4da7d2e938cf7d0
Feke_21 <favouredubio@gmail.com>
2023-06-19 15:12:55 +0000

    Trying to fix errors

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,7 +8,7 @@ from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-if __name__ == '__main__':
+if __name__ == "__main__":
      engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
