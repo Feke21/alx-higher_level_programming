2059a30d0d5e1cddaf8201fbc54de67115fc2bf7
Feke_21 <favouredubio@gmail.com>
2023-07-02 07:57:13 -0700

    Fixing file accord. to pycodestyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-displays the value of the X-Request-Id variable found in the header of the response.
+display value of the X-Request-Id var in header of the response.
 """
 
 
