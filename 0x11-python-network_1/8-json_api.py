ca9882d363d38ea421fb56834d53ed9c254920e9
Feke_21 <favouredubio@gmail.com>
2023-07-02 07:56:14 -0700

    Fixing file accord. to pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in a letter, sends a POST request to 
+"""takes in a letter, sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     from sys import argv
 
     q = argv[1] if len(argv) > 1 else ""
-    r = post('http://0.0.0.0:5000/search_user', data={'q':q})
+    r = post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
         response = r.json()
-        if response =={}:
+        if response == {}:
             print('No result')
         else:
             print("[{}] {}".format(response.get('id'), response.get('name')))
